9c29a38e5526870ed4765b93b70a93df75d68630
SeaLife <mircotries@hotmail.de>
2014-02-08 23:06:59 +0100

    Update file_list.lua

--- file_list.lua
+++ file_list.lua
@@ -1,1 +1,12 @@
 -- Installer File-List
+fs.makeDir("/etc/bin")
+fs.makeDir("/etc/sbin")
+fs.makeDir("/etc/usr")
+fs.makeDir("/home/root")
+
+INSTALL_FILES = {}
+
+INSTALL_FILES[1] = "etc/bin/apt-get.lua"
+INSTALL_FILES[2] = "etc/bin/shell.lua"
+INSTALL_FILES[3] = "etc/bin/user.lua"
+INSTALL_FILES[4] = "etc/bin/user_globals.lua"
